3643467c0e3f811c17cf1f2d8427954420ce0d54
Nicel193 <nicelgoog@gmail.com>
2023-09-30 01:40:15 +0300

    Add ange card algotitm

--- client/js/testScene.js
+++ client/js/testScene.js
@@ -50,10 +50,11 @@ export default class mainScene extends Phaser.Scene {
         var centerY = this.cameras.main.height;
 
         var cardSize = 0.2;
-        var numCards = 5;
+        var numCards = 6;
         var totalWidth = 0;
+        var anglePos = -1;
         for (var i = 1; i < numCards; i++) {
-            totalWidth += (this.textures.getFrame('card').width * cardSize);
+            totalWidth += ((this.textures.getFrame('card').width / 2) * cardSize);
         }
 
         for (var i = 0; i < numCards; i++) {
@@ -61,12 +62,33 @@ export default class mainScene extends Phaser.Scene {
             card.setScale(cardSize);
             card.setOrigin(0.5, 0.5);
 
-            var targetPosition = centerX + ((card.width * cardSize) * i) - (totalWidth / 2);
+            var targetPosition = centerX + (((card.width / 2) * cardSize) * i) - (totalWidth / 2);
+            var t;
+
+            switch (numCards) {
+                case 0:
+                    t = 0;
+                    break;
+                default:
+                    t = 1 / (numCards - 1) * 2;
+                    break;
+            }
+
+            var angleInRadians = Math.atan2(1, anglePos / 4);
+            var angleInDegrees = (180 / Math.PI) * -angleInRadians;
+            var positionY = ((angleInDegrees + 90) * 4); 
+
+            if(positionY > 0) positionY *= -1;
+
+            console.log(positionY);
+
+            anglePos += t;
 
             this.tweens.add({
                 targets: card,
                 x: targetPosition,
-                y: centerY,
+                y: (centerY - positionY) - 25,
+                angle: angleInDegrees + 90,
                 ease: 'sine',
                 duration: 1000,
                 delay: i * 200
